6975161d289c578c221437be28afe4410212d35f
mglava2005 <magdy.designer1@gmail.com>
2024-08-31 22:39:17 +0300

    Edit home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -195,3 +195,21 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach((counter) => {
+  counter.innerText = "0";
+  const updateCounter = () => {
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else counter.innerText = target;
+  };
+  updateCounter();
+});
